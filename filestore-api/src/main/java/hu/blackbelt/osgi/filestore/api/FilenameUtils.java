718b3e985235efe0b45af5807b7dcafe35c46f5d
Robert Csakany <csakany.robert@gmail.com>
2024-04-05 11:45:57 +0200

    Fix review issue

--- filestore-api/src/main/java/hu/blackbelt/osgi/filestore/api/FilenameUtils.java
+++ filestore-api/src/main/java/hu/blackbelt/osgi/filestore/api/FilenameUtils.java
@@ -5,23 +5,23 @@ import lombok.NonNull;
 public class FilenameUtils {
     @NonNull
     public static String makeValidFilename(String fileName) {
-        String fn = fileName.trim();
-        while (fn.contains("  ")) {
-            fn = fn.replaceAll("  ", " ");
-        }
-        while (fn.contains("..")) {
-            fn = fn.replaceAll("\\.\\.", ".");
-        }
-        while (fn.endsWith(".")) {
-            fn = fn.replaceAll("[.]$", "");
-        }
-        fn = fn.trim();
+        String fn = fileName.trim()
+            .replaceAll("^[aA][uU][xX]$|^[cC][lL][oO][cC][kK]$|^[cC][oO][nN]$|^[nN][uU][lL]$|^[pP][rR][nN]$|^[cC][oO][mM][1-9]$|^[lL][pP][tT][1-9]$", "reserved")
+            .replaceAll("[$()+=\\[\\];#@~,&']", "")
+            .replaceAll("[ ][.]+", ".")
+            .replaceAll("[.][ ]+", ".")
+            .replaceAll("\\.+", ".")
+            .replaceAll("\\s+", " ")
+            .trim()
+            .replaceAll("[.]$", "");
 
-        fn = fn.replaceAll("^[aA][uU][xX]$|^[cC][lL][oO][cC][kK]$|^[cC][oO][nN]$|^[nN][uU][lL]$|^[pP][rR][nN]$|^[cC][oO][mM][1-9]$|^[lL][pP][tT][1-9]$", "reserved")
-                .replaceAll("[$()+=\\[\\];#@~,&']", "");
         if (fn.length() > 128) {
             return fn.substring(0, 127);
         }
         return fn;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(makeValidFilename("Hdshddas .....         asxasd...     .dd.... "));
     }
 }
